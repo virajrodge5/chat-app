72128853054b319d0b7bd33ed40a344a28706cef
Viraj Rodge <99235371+virajrodge5@users.noreply.github.com>
2024-03-16 20:57:52 +0530

    Added portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
-import { Fragment } from "react"
+import { Fragment } from "react";
 
 export const Footer = () => {
     return (
         <Fragment>
-            <div class="footer-padding">
-                <div class="footer">
-                    <a href="https://portfolio-virajrodge5.vercel.app/"><p>viraj.R</p></a>
+            <div className="footer-padding">
+                <div className="footer">
+                    <a href="https://portfolio-virajrodge5.vercel.app/" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none', cursor: 'pointer' }}>
+                        <p>viraj.R</p>
+                    </a>
                 </div>
             </div>
         </Fragment>
